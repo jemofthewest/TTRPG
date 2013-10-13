91cfb52cedf202e6cac70d01a68e4d48ce0e1513
Jeremy <jemofthewest@gmail.com>
2013-10-13 17:54:20 -0400

    Added debug code
    Only has "error" function for now

---
+++ debug.c
@@ -0,0 +1,17 @@
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#include <stdlib.h>
+
+void
+error (lua_State *L, const char *fmt, ... )
+{
+  va_list argp;
+  va_start(argp, fmt);
+  vfprintf(stderr, fmt, argp);
+  va_end(argp);
+  lua_close(L);
+  exit(EXIT_FAILURE);
+}
+
---
+++ debug.h
@@ -0,0 +1,7 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+void
+error ( lua_State *L, const char *fmt, ... );
+
+#endif
